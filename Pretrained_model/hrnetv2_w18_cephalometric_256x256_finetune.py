f1d8d9b35573051522035cb4d3b4e043504a9093
Mohamed Maher <mohamedmaher@mohameds-air.speedport.ip>
2025-05-31 15:34:50 +0200

    fixxes

--- Pretrained_model/hrnetv2_w18_cephalometric_256x256_finetune.py
+++ Pretrained_model/hrnetv2_w18_cephalometric_256x256_finetune.py
@@ -106,7 +106,7 @@ val_dataloader = dict(
 test_dataloader = val_dataloader # Often, test and val dataloaders are configured similarly
 
 # Evaluators
-val_evaluator = dict(type='NME', norm_mode='keypoint_distance', keypoint_indices=list(range(19))) # Adjust keypoint_indices if needed
+val_evaluator = dict(type='NME', norm_mode='keypoint_distance', keypoint_indices=[0, 1])
 test_evaluator = val_evaluator
 
 # You might want to adjust learning rate for fine-tuning
